10c3f03ad89e1f039e4b5ba2b42268e62dee1fd9
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2022-10-19 19:48:08 -0300

    Update Dio.py

--- Dio.py
+++ Dio.py
@@ -1,8 +1,11 @@
-# distancia = float(100)
-#diametro1 = float(2)
-#diametro2 = float(2)
-#icm = distancia / (diametro1 + diametro2)
-#print(f"o ICM é {icm:.2f}")s
+#A entrada é composta por 3 inteiros, N(0 < N < 10000), X e Y(0 < X, Y < 100), que indicam,
+#respectivamente, a distância entre os Palantír, o diâmetro do Palantír de Sauron e o diâmetro do Palantír de Saruman.
+
+#Saída
+#Um valor real indicando o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais.
+#Exemplos de Entrada	Exemplos de Saída
+#200 3 8	
+#18.18
 
 distancia = float(input("digite a distancia: ") or 200)
 diametro1 = float(input("digite o diametro 1: ") or 3)
