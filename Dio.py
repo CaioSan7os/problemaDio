c81d9aea18f625f7ab2d109539e2f961dd2f71a0
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2022-10-19 19:42:27 -0300

    Create Dio.py
    meu código n está sendo aceito, ainda não sei o porquê :c

---
+++ Dio.py
@@ -0,0 +1,11 @@
+# distancia = float(100)
+#diametro1 = float(2)
+#diametro2 = float(2)
+#icm = distancia / (diametro1 + diametro2)
+#print(f"o ICM é {icm:.2f}")s
+
+distancia = float(input("digite a distancia: ") or 200)
+diametro1 = float(input("digite o diametro 1: ") or 3)
+diametro2 = float(input("digite o diametro 2: ") or 8)
+icm = distancia / (diametro1 + diametro2)
+print(f"o ICM é {icm:.2f}")
